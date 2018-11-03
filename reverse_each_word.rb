2a8fd190c2698e824f8d8d3e29d04260753aacde
Greg Morgan <gregm9999@gmail.com>
2018-11-03 01:58:10 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,10 @@
 def reverse_each_word(sentence)
   myAr=[]
-  myAr2=[]
   myAr2=sentence.split("").reverse.join("")
-  puts myAr2
-  myAr2.each_with_index do |word, index| myAr.unshift(myAr2[index])
-    # word.join(" ")
-    # myAr.push(word)
-  end
-   return myAr
+  # puts myAr2
+  # myAr2.each_with_index do |word, index| myAr.unshift(myAr2[index])
+  #   # word.join(" ")
+  #   # myAr.push(word)
+  # end
+   return myAr2
 end
