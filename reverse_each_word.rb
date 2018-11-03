4fac24ef3ae492370e29998ca874c5420baf9675
Greg Morgan <gregm9999@gmail.com>
2018-11-03 02:36:50 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,9 +3,10 @@ def reverse_each_word(sentence)
   myAr=[]
   myAr2=sentence.split("").reverse.join("")
   puts myAr2
-  myAr2.split(" ").each_with_index do |word, index| myAr.push(myAr2[index])
-    # binding.pry
-  end
+  myAr=myAr2.split(" ").reverse.join("")
+  # myAr2.split(" ").each_with_index do |word, index| myAr.push(myAr2[index])
+  #   # binding.pry
+  # end
   # puts myAr2
   # myAr2.each_with_index do |word, index| myAr.unshift(myAr2[index])
   #   # word.join(" ")
