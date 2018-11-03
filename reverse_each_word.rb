61a7f0125dde4e09be5f59bd4712ee4e7b708a27
Greg Morgan <gregm9999@gmail.com>
2018-11-03 01:50:34 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   myAr=[]
   myAr2=sentence.split(" ")
   puts myAr2
-  myAr2.each_with_index do |word, index| word[index].push(myAr2[index])
+  myAr2.each_with_index do |word, index| myAr2.push(word[index])
     word.join(" ")
     myAr.push(word)
   end
